dd6b76409f55688108bcabecdebfa53fa3212f84
Anthony Bouch <tony@58bits.com>
2012-04-03 23:52:37 +0700

    Added auto_set_breadcrumb method to controller_helper

--- lib/ariane/rails/controller_helper.rb
+++ lib/ariane/rails/controller_helper.rb
@@ -1,6 +1,21 @@
 module Ariane
   # Adds the ariane helper to controllers.
   module ControllerHelper
+
+    # Internal: Sets a hash of cumb levels
+    #
+    # Sets a hash of cumb levels for actions that require custom levels
+    #
+    # Returns nothing.
+    def crumb_levels(levels)
+      @crumb_levels = levels
+    end
+
+    # Internal: Sets the Ariane environment
+    #
+    # Sets the Ariane per request environment as well assigns the session hash.
+    #
+    # Returns Ariane.breadcrumb
     def ariane
       # Set environment and a reference to the session 
       if !Ariane.request_env || Ariane.request != request.object_id
@@ -10,6 +25,42 @@ module Ariane
       end
 
       Ariane.breadcrumb
+    end
+
+    # Public: Controller helper method for auto creation of breadcrums.
+    #
+    # Automatically sets the breadcrumb based on controller and action names.
+    #
+    # Returns nothing
+    def auto_set_breadcrumb
+      if ariane.crumbs.empty? || ariane.crumbs.length == 0
+        ariane.add("Home", root_path, 1)
+      end
+
+      if self.action_name == "index"
+        name = self.controller_name.gsub(/_/, " ").capitalize
+        level = set_level(2)
+      else
+        name = self.action_name.capitalize + " " + self.controller_name.singularize.gsub(/_/, " ").capitalize
+        level = set_level(3)
+      end
+
+      ariane.add(name, request.fullpath, level)
+    end
+
+
+    private
+
+    # Private: Helper method to retrieve a crumb level from the @crumb_levels hash 
+    # if it exists.
+    #
+    # Returns level
+    def set_level(default)
+      if @crumb_levels
+        return @crumb_levels[self.action_name.to_sym] ? @crumb_levels[self.action_name.to_sym] : default
+      else
+        return default
+      end
     end
   end
 end
