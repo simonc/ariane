49204efdaf67b242427d5865ff916a45a60e54a1
Michael Durrant <m2@snap2web.com>
2013-11-05 16:53:18 -0500

    rspec expect() syntax, plus using 'match()' over '=~' and 'eq' over '=='

--- spec/ariane/render/html_spec.rb
+++ spec/ariane/render/html_spec.rb
@@ -5,26 +5,26 @@ module Ariane
 
     describe HTML do
       it "has an output_buffer" do
-        HTML.new.respond_to?(:output_buffer)
+        expect(HTML.new.respond_to?(:output_buffer)).to be_true
       end
 
       it "has a setter for the output_buffer" do
-        HTML.new.respond_to?(:"output_buffer=")
+        expect(HTML.new.respond_to?(:"output_buffer=")).to be_true
       end
 
       describe "#initialize" do
         it "sets default options" do
-          HTML.new.options[:list_class].should == 'breadcrumb'
+          expect(HTML.new.options[:list_class]).to eq 'breadcrumb'
         end
 
         it "merges the options passed as argument to the default ones" do
           html = HTML.new(list_id: 'test')
-          html.options[:list_id].should    == 'test'
-          html.options[:list_class].should == 'breadcrumb'
+          expect(html.options[:list_id]).to eq 'test'
+          expect(html.options[:list_class]).to eq 'breadcrumb'
         end
 
         it "calls uses Base default options" do
-          HTML.new.options[:divider].should == ' / '
+          expect(HTML.new.options[:divider]).to eq ' / '
         end
       end
 
@@ -32,20 +32,20 @@ module Ariane
         it "returns an html_safe string" do
           html = HTML.new
           html.stub(:list).and_return('test')
-          html.render([]).html_safe?.should be_true
+          expect(html.render([]).html_safe?).to be_true
         end
       end
 
       describe "#list" do
         it "returns an HTML paragraph with list_id and list_class" do
           html = HTML.new(list_id: 'test')
-          html.list([]).should == '<p class="breadcrumb" id="test"></p>'
+          expect(html.list([])).to eq '<p class="breadcrumb" id="test"></p>'
         end
 
         it "returns an HTML paragraph containing crumbs" do
           html   = HTML.new
           crumbs = [Crumb.new('text1', 'url1'), Crumb.new('text2', 'url2')]
-          html.list(crumbs).should == '<p class="breadcrumb"><a href="url1">text1</a> / text2</p>'
+          expect(html.list(crumbs)).to eq '<p class="breadcrumb"><a href="url1">text1</a> / text2</p>'
         end
       end
 
@@ -53,14 +53,14 @@ module Ariane
         it "returns all crumbs in a formatted form" do
           html   = HTML.new
           crumbs = [Crumb.new('text1', 'url1'), Crumb.new('text2', 'url2')]
-          html.items(crumbs).should == '<a href="url1">text1</a> / text2'
+          expect(html.items(crumbs)).to eq '<a href="url1">text1</a> / text2'
         end
 
         it "sets the active flag for the last item" do
           html   = HTML.new
           crumbs = [Crumb.new('text', 'url'), Crumb.new('text', 'url')]
-          html.should_receive(:item).with(crumbs.first, false).and_return('')
-          html.should_receive(:item).with(crumbs.last, true).and_return('')
+          expect(html).to receive(:item).with(crumbs.first, false).and_return('')
+          expect(html).to receive(:item).with(crumbs.last, true).and_return('')
           html.items(crumbs)
         end
       end
@@ -72,20 +72,20 @@ module Ariane
         end
 
         it "returns the crumb in a formatted form" do
-          @html.item(@crumb).should =~ %r[^<a href="url">text</a>]
+          expect(@html.item(@crumb)).to match(%r[^<a href="url">text</a>])
         end
 
         it "appends the divider to the crumb" do
-          @html.item(@crumb).should =~ %r[ / $]
+          expect(@html.item(@crumb)).to match(%r[ / $])
         end
 
         it "does not append the divider if the crumb is active" do
-          @html.item(@crumb, true).should_not =~ %r[ / $]
+          expect(@html.item(@crumb, true)).to_not match(%r[ / $])
         end
 
         it "uses the divider from options, not the divider method" do
           @html.options[:divider] = 'custom'
-          @html.item(@crumb).should =~ /custom/
+          expect(@html.item(@crumb)).to match(/custom/)
         end
       end
 
@@ -96,50 +96,50 @@ module Ariane
         end
 
         it "returns the link for the crumb" do
-          @html.link(@crumb).should == '<a href="url">text</a>'
+          expect(@html.link(@crumb)).to eq '<a href="url">text</a>'
         end
 
         it "sets the link class" do
           @html.options[:link_class] = 'test'
-          @html.link(@crumb).should =~ /<a /
-          @html.link(@crumb).should =~ /class\=\"test\"/
-          @html.link(@crumb).should =~ /href\=\"url\"/
-          @html.link(@crumb).should =~ /text\<\/a\>/
+          expect(@html.link(@crumb)).to match(/<a /)
+          expect(@html.link(@crumb)).to match(/class\=\"test\"/)
+          expect(@html.link(@crumb)).to match(/href\=\"url\"/)
+          expect(@html.link(@crumb)).to match(/text\<\/a\>/)
         end
 
         it "sets the active class when the crumb is active and link_active is true" do
           @html.options[:link_active] = true
-          @html.link(@crumb, true).should =~ /<a /
-          @html.link(@crumb, true).should =~ /class\=\"active\"/
-          @html.link(@crumb, true).should =~ /href\=\"url\"/
-          @html.link(@crumb, true).should =~ /text\<\/a\>/
+          expect(@html.link(@crumb, true)).to match(/<a /)
+          expect(@html.link(@crumb, true)).to match(/class\=\"active\"/)
+          expect(@html.link(@crumb, true)).to match(/href\=\"url\"/)
+          expect(@html.link(@crumb, true)).to match(/text\<\/a\>/)
         end
 
         it "returns a link if the crumb has an url" do
-          @html.link(@crumb).should == '<a href="url">text</a>'
+          expect(@html.link(@crumb)).to eq '<a href="url">text</a>'
         end
 
         it "returns a link if the crumb is active and link_active is true" do
           @html.options[:link_active] = true
-          @html.link(@crumb, true).should =~ /<a /
+          expect(@html.link(@crumb, true)).to match(/<a /)
         end
 
         it "returns crumb's text if the crumb has no url" do
           @crumb.url = nil
-          @html.link(@crumb).should == @crumb.text
+          expect(@html.link(@crumb)).to eq @crumb.text
         end
 
         it "returns crumb's text if the crumb is active and link_active is false" do
-          @html.link(@crumb, true).should == @crumb.text
+          expect(@html.link(@crumb, true)).to eq @crumb.text
         end
 
         it "returns an html_safe string when it returns a link" do
-          @html.link(@crumb).html_safe?.should be_true
+          expect(@html.link(@crumb).html_safe?).to be_true
         end
 
         it "returns an html_safe string when it returns text" do
           @crumb.url = nil
-          @html.link(@crumb).html_safe?.should be_true
+          expect(@html.link(@crumb).html_safe?).to be_true
         end
       end
     end
--- spec/ariane_spec.rb
+++ spec/ariane_spec.rb
@@ -14,55 +14,55 @@ describe Ariane do
       Ariane.configure do |config|
         config.default_renderer = 'test_renderer'
       end
-      Ariane.default_renderer.should == 'test_renderer'
+      expect(Ariane.default_renderer).to eq 'test_renderer'
     end
   end
 
-  # describe "#request_env=" do
-  #   it "sets the breacrumb env variable" do
-  #     Ariane.request_env = {}
-  #     Ariane.request_env[:breadcrumb].should_not be_nil
-  #   end
-  # end
+   describe "#request_env=" do
+     it "sets the breadcrumb request env variable" do
+       Ariane.request_env = {}
+       expect(Ariane.request_env[:breadcrumb]).to_not eq({})
+     end
+   end
 
   describe "#breadcrumb" do
     it "returns the breadcrumb from the request_env" do
       Ariane.request_env[:breadcrumb] = "test_breadcrumb"
-      Ariane.breadcrumb.should == "test_breadcrumb"
+      expect(Ariane.breadcrumb).to eq "test_breadcrumb"
     end
   end
 
   describe "#breadcrumb=" do
     it "sets the breadcrumb in the request_env" do
       Ariane.breadcrumb = "test_breadcrumb"
-      Ariane.request_env[:breadcrumb].should == "test_breadcrumb"
+      expect(Ariane.request_env[:breadcrumb]).to eq "test_breadcrumb"
     end
   end
 
   describe "#default_renderer" do
     it "returns the default renderer" do
       Ariane.default_renderer = "test_renderer"
-      Ariane.default_renderer.should == "test_renderer"
+      expect(Ariane.default_renderer).to eq "test_renderer"
     end
   end
 
   describe "#default_renderer=" do
     it "instanciates the renderer if a class is passed" do
       Ariane.default_renderer = String
-      Ariane.default_renderer.should == ""
+     expect(Ariane.default_renderer).to eq ""
     end
   end
 
   describe "#dynamic_breadcrumb" do
     it "returns the default mode which is false" do
-      Ariane.dynamic_breadcrumb.should be_false
+      expect(Ariane.dynamic_breadcrumb).to be_false
     end
   end
 
   describe "#dynamic_breadcrumb=" do
     it "returns the option set" do
       Ariane.dynamic_breadcrumb = true
-      Ariane.dynamic_breadcrumb.should be_true
+      expect(Ariane.dynamic_breadcrumb).to be_true
     end
   end
 end
